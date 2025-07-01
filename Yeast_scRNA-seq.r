e69528d07c25218e2b5ef4eb6315cda2d48a510b
Mrugakshi Chidrawar <55922369+mrugakshi77@users.noreply.github.com>
2025-06-30 22:08:44 -0700

    Update Yeast_scRNA-seq.r

--- Yeast_scRNA-seq.r
+++ Yeast_scRNA-seq.r
@@ -1,8 +1,8 @@
-This project reproduces key findings from Gasch et al. (2017) by analyzing single-cell RNA-seq data from Saccharomyces cerevisiae under stressed and unstressed conditions. Using SMART-seq-based expression profiles, we explore regulatory heterogeneity by examining gene modules such as ribosomal protein (RP) and induced Environmental Stress Response (iESR) genes. The analysis includes log-transformed normalization, mean-centering, module scoring and visualization of cell-to-cell variation in stress-responsive gene expression.
+# This project reproduces key findings from Gasch et al. (2017) by analyzing single-cell RNA-seq data from Saccharomyces cerevisiae under stressed and unstressed conditions. Using SMART-seq-based expression profiles, we explore regulatory heterogeneity by examining gene modules such as ribosomal protein (RP) and induced Environmental Stress Response (iESR) genes. The analysis includes log-transformed normalization, mean-centering, module scoring and visualization of cell-to-cell variation in stress-responsive gene expression.
 
-Conducted by Mrugakshi Chidrawar,
-Bioinformatician | M.S. in Bioinformatics, Boston University
-Expertise: NGS analysis, scalable pipeline development, variant interpretation
+# Conducted by Mrugakshi Chidrawar,
+# Bioinformatician | M.S. in Bioinformatics, Boston University
+# Expertise: NGS analysis, scalable pipeline development, variant interpretation
 # ================================
 # 0. Load Required Libraries
 # ================================
